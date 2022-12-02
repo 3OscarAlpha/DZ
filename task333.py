f5ecec467960d0292b9f62d77c68101ac2c75d62
SteveJop228 <olesya.zemtsova@gmail.com>
2022-12-02 19:57:07 +0300

    com

--- task333.py
+++ task333.py
@@ -8,12 +8,41 @@ from tkinter import scrolledtext
 from tkinter import messagebox
 
 
+# def get_text():
+#     s = entry.get(1.0, END)
+#     label['text'] = s
 
-def clicked():
-    global words
-    words = entry.get()
-    print(words)
-    return(words)
+def click():
+    global scidata, ver, hor, fig
+    source = entr1.get()
+    hdulist = pyfits.open(f"{source}")
+    hdulist.info()
+    scidata = hdulist[0].data
+    hdulist.close()
+    fig = plt.figure()
+    ver = fig.add_subplot(1, 2, 1) #колво строк, столбцов, номер позиции
+    hor = fig.add_subplot(1, 2, 2)
+
+def ver_graph:
+    # # профиль вертикальный (x const)
+    damn = [row[730] for row in scidata]
+    print('damn = ', damn)
+    ver_y = damn[1875:1910]
+    ver_x = [i for i in range(1875, 1910)]
+    ver.set(title = 'Вертикальный профиль')
+    ver.plot(ver_x, ver_y)
+
+def hor_graph: #профиль горизонтальный (y const)
+    hor_x = [i for i in range(724, 738)]
+    hor_y = scidata[1891][724:738]
+    print(hor_x, hor_y)
+    hor.plot(hor_x, hor_y, color = '#8b00ff')
+    hor.set(title = 'Горизонтальный профиль')
+    plt.show()
+
+
+def handle_click(event):
+    print("Нажата кнопка!")
 
 # def select():
 #     lbl.configure(text=str(selected.get()))
@@ -22,53 +51,60 @@ def clicked():
 #     messagebox.showinfo('Окно всплыло', 'Работает!')
 
 
-
-
-# x_coord = float(input('Введите координату х: '))
-# y_coord = float(input('Введите координату y: '))
-
 x_coord = 730
 y_coord = 1891
 
 star_window = tk.Tk()
 star_window.title(f"Star with x:{x_coord}, y:{y_coord}")
-star_window.geometry('600x600') #почему не в юнитах выдает?
+star_window.geometry('800x600') #почему не в юнитах выдает?
 
-lbl = Label(star_window, text='Добро пожаловать в программу по взлому мира', foreground = 'white', background = 'black')
-lbl.pack()
+# lbl = Label(star_window, text='Добро пожаловать в программу по взлому мира', foreground = 'white', background = 'black')
+# lbl.pack()
 
-button = Button(text = 'Горизонтальный профиль', bg = 'grey') # width=25, height=5, bg="blue", fg="yellow"
-button.pack()
+# button = Button(text = 'Горизонтальный профиль', bg = 'grey') # width=25, height=5, bg="blue", fg="yellow"
+# button.pack()
 
-entry = Entry(text = 'Vvedite put', bg='yellow', width = 50)
-entry.pack()
-btn = Button(star_window, text="Клик!", command=clicked)
-btn.pack()
+lbl1 = Label(text='Vvedite puuuuut')
+lbl1.grid(column = 1, row = 1)
+entr1 = Entry(text = 'Vvedite put', bg='black', fg = 'white', width = 50)
+entr1.grid(column = 2, row = 1)
 
-print(words)
+lbl2 = Label(text='X')
+lbl2.grid(column = 1, row = 2)
+entr2 = Entry(width = 30)
+entr2.grid(row = 2, column = 2)
 
-#                    Построение графиков
-hdulist = pyfits.open(f"{words}")
-hdulist.info()
-scidata = hdulist[0].data
-fig = plt.figure()
-ver = fig.add_subplot(1, 2, 1) #колво строк, столбцов, номер позиции
-hor = fig.add_subplot(1, 2, 2)
-# профиль вертикальный (x const)
-damn = [row[730] for row in scidata]
-print('damn = ', damn)
-ver_y = damn[1875:1910]
-ver_x = [i for i in range(1875, 1910)]
-ver.set(title = 'Вертикальный профиль')
-ver.plot(ver_x, ver_y)
-#профиль горизонтальный (y const)
-hor_x = [i for i in range(724, 738)]
-hor_y = scidata[1891][724:738]
-print(hor_x, hor_y)
-hor.plot(hor_x, hor_y, color = '#8b00ff')
-hor.set(title = 'Горизонтальный профиль')
-plt.show()
-hdulist.close()
+lbl3 = Label(text='Y')
+lbl3.grid(column = 1, row = 3)
+entr3 = Entry(width = 30)
+entr3.grid(row = 3, column = 2)
+
+lbl4 = Label(text='R of star')
+lbl4.grid(column = 3, row = 2)
+entr4 = Entry(width = 30)
+entr4.grid(row = 2, column = 4)
+
+
+lbl4 = Label(text='R of back1')
+lbl4.grid(column = 3, row = 3)
+entr4 = Entry(width = 30)
+entr4.grid(row = 3, column = 4)
+
+
+lbl4 = Label(text='R of back2')
+lbl4.grid(column = 3, row = 4)
+entr4 = Entry(width = 30)
+entr4.grid(row = 4, column = 4)
+
+
+# Button(star_window, text="Принять",
+#        command=get_text).pack(side=LEFT)
+btn = Button(star_window, text="Принять", command=click)
+btn.grid(row = 5, column = 1)
+# btn3 = Button(text = 'Click here')
+# btn3.pack()
+# btn3.bind("<Button-1>", handle_click)
+
 
 star_window.mainloop()
 #
@@ -110,3 +146,10 @@ star_window.mainloop()
 #
 # # btn3 = Button(star_window, text='Вызвать окно', command=mess)
 # # btn3.grid(column=7, row=0)
+
+
+
+
+#
+# #                    Построение графиков
+# hdulist.close()
