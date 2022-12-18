86c4a0cacaeee71c4942fd98dcd207c00e322252
SteveJop228 <olesya.zemtsova@gmail.com>
2022-12-18 14:21:18 +0300

    com

--- task333.py
+++ task333.py
@@ -1,11 +1,15 @@
 import astropy.io.fits as pyfits
 import astropy
 import matplotlib.pyplot as plt
-import numpy
+import numpy as np
 import tkinter as tk
 from tkinter import *
+import pylab #для xlim
 from tkinter import scrolledtext
 from tkinter import messagebox
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_tkagg import NavigationToolbar2Tk
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 
 # def get_text():
@@ -13,42 +17,95 @@ from tkinter import messagebox
 #     label['text'] = s
 
 def click():
-    global scidata, ver, hor, fig
-    source = entr1.get()
+    global scidata, fig, coord_x # X=730, Y=1890
+    global coord_y, r_of_star
+    source = entr1.get() #C:/Users/3512/Downloads/Telegram Desktop/v523cas60s-001.fit
+    coord_x = entr2.get()
+    coord_y = entr3.get()
+    r_of_star = entr4.get()
+    r1_back = float(entr5.get())
+    r2_back = float(entr6.get())
     hdulist = pyfits.open(f"{source}")
     hdulist.info()
     scidata = hdulist[0].data
     hdulist.close()
-    fig = plt.figure()
-    ver = fig.add_subplot(1, 2, 1) #колво строк, столбцов, номер позиции
-    hor = fig.add_subplot(1, 2, 2)
 
-def ver_graph:
-    # # профиль вертикальный (x const)
-    damn = [row[730] for row in scidata]
-    print('damn = ', damn)
-    ver_y = damn[1875:1910]
-    ver_x = [i for i in range(1875, 1910)]
-    ver.set(title = 'Вертикальный профиль')
-    ver.plot(ver_x, ver_y)
+    def ver_plot():
 
-def hor_graph: #профиль горизонтальный (y const)
-    hor_x = [i for i in range(724, 738)]
-    hor_y = scidata[1891][724:738]
-    print(hor_x, hor_y)
-    hor.plot(hor_x, hor_y, color = '#8b00ff')
-    hor.set(title = 'Горизонтальный профиль')
-    plt.show()
+        # damn = [row[730] for row in scidata]
+        damn = [row[int(coord_x)] for row in scidata]
+        left = int(coord_y) - int(r_of_star)
+        right = int(coord_y) + int(r_of_star)
+        print(left)
+        ver_y = damn[left:right]
+        print(ver_y)
+        ver_x = [i for i in range(left, right)]
+
+        fig = Figure(figsize=(6,6), dpi=70)
+        plot1 = fig.add_subplot(111)
+        plot1.plot(ver_x, ver_y)
+        # plot1.set_xlim([left, right])
+        canvas = FigureCanvasTkAgg(fig, master=star_window)
+        canvas.draw()
+        canvas.get_tk_widget().grid(column=1, row =7)
+
+    def hor_plot():  # профиль горизонтальный (y const)
+        hor_x = [i for i in range(724, 738)]
+        hor_y = scidata[1891][724:738]
+        fig = Figure(figsize=(5,5), dpi=70)
+        plot1 = fig.add_subplot(111)
+        plot1.plot(hor_x, hor_y)
+        canvas = FigureCanvasTkAgg(fig, master=star_window)
+        canvas.draw()
+        canvas.get_tk_widget().grid(column=2, row =7)
+
+    def d_plot():
+        z_temp, Z = [], []
+        i = 1880
+        while i < 1905:
+            for k in range(720, 745):
+                z_temp.append(scidata[i][k]) #type = list
+            z_arr = np.asarray(z_temp, dtype=int) #type = ndarray
+            Z.append(z_arr)
+            z_temp = []
+            i = i + 1
+        Z = np.asarray(Z)
+
+        x = [i for i in range(1880, 1905)]
+        y = [i for i in range(720, 745)]
+        X, Y = np.meshgrid(x, y)
+        fig = plt.Figure(figsize=(5,5), dpi=70)
+        # ax = plt.axes(projection='3d') #scatters
+        ax = fig.add_subplot(111, projection='3d')
+        ax.plot_surface(X, Y, Z, cmap='inferno')
+        ax.set_xlabel('x')
+        ax.set_ylabel('y')
+        ax.set_zlabel('z')
+        canvas = FigureCanvasTkAgg(fig, master=star_window)
+        canvas.draw()
+        canvas.get_tk_widget().grid(column=3, row =7)
 
 
-def handle_click(event):
-    print("Нажата кнопка!")
 
-# def select():
-#     lbl.configure(text=str(selected.get()))
-# # #
-# def mess():
-#     messagebox.showinfo('Окно всплыло', 'Работает!')
+
+    var1, var2, var3 = IntVar(), IntVar(), IntVar()
+    var1.set(1)
+    c1 = Checkbutton(star_window, text='Vertical',
+                     variable=var1, onvalue=1, offvalue=0,
+                     command=ver_plot)
+    c1.grid(column=1, row=6)
+
+    var2.set(2)
+    c2 = Checkbutton(star_window, text='Horizontal',
+                     variable=var2, onvalue=1, offvalue=0,
+                     command=hor_plot)
+    c2.grid(column=2, row=6)
+
+    var3.set(3)
+    c3 = Checkbutton(star_window, text='3d',
+                     variable=var3, onvalue=1, offvalue=0,
+                     command=d_plot)
+    c3.grid(column=3, row=6)
 
 
 x_coord = 730
@@ -58,98 +115,47 @@ star_window = tk.Tk()
 star_window.title(f"Star with x:{x_coord}, y:{y_coord}")
 star_window.geometry('800x600') #почему не в юнитах выдает?
 
-# lbl = Label(star_window, text='Добро пожаловать в программу по взлому мира', foreground = 'white', background = 'black')
-# lbl.pack()
-
-# button = Button(text = 'Горизонтальный профиль', bg = 'grey') # width=25, height=5, bg="blue", fg="yellow"
-# button.pack()
 
 lbl1 = Label(text='Vvedite puuuuut')
 lbl1.grid(column = 1, row = 1)
-entr1 = Entry(text = 'Vvedite put', bg='black', fg = 'white', width = 50)
+entr1 = Entry(text = 'Vvedite put', bg='black', fg = 'white', width = 70)
 entr1.grid(column = 2, row = 1)
+entr1.insert(0, "C:/Users/3512/Downloads/Telegram Desktop/v523cas60s-001.fit")
 
 lbl2 = Label(text='X')
 lbl2.grid(column = 1, row = 2)
 entr2 = Entry(width = 30)
 entr2.grid(row = 2, column = 2)
+entr2.insert(0, '730')
 
 lbl3 = Label(text='Y')
 lbl3.grid(column = 1, row = 3)
 entr3 = Entry(width = 30)
 entr3.grid(row = 3, column = 2)
+entr3.insert(0, '1891')
 
 lbl4 = Label(text='R of star')
 lbl4.grid(column = 3, row = 2)
 entr4 = Entry(width = 30)
 entr4.grid(row = 2, column = 4)
+entr4.insert(0, '10')
+
+lbl5 = Label(text='R of back1')
+lbl5.grid(column = 3, row = 3)
+entr5 = Entry(width = 30)
+entr5.grid(row = 3, column = 4)
+entr5.insert(0, '5')
+
+lbl6 = Label(text='R of back2')
+lbl6.grid(column = 3, row = 4)
+entr6 = Entry(width = 30)
+entr6.grid(row = 4, column = 4)
+entr6.insert(0, '6')
 
 
-lbl4 = Label(text='R of back1')
-lbl4.grid(column = 3, row = 3)
-entr4 = Entry(width = 30)
-entr4.grid(row = 3, column = 4)
 
-
-lbl4 = Label(text='R of back2')
-lbl4.grid(column = 3, row = 4)
-entr4 = Entry(width = 30)
-entr4.grid(row = 4, column = 4)
-
-
-# Button(star_window, text="Принять",
-#        command=get_text).pack(side=LEFT)
 btn = Button(star_window, text="Принять", command=click)
 btn.grid(row = 5, column = 1)
-# btn3 = Button(text = 'Click here')
-# btn3.pack()
-# btn3.bind("<Button-1>", handle_click)
 
 
 star_window.mainloop()
-#
-# lbl = Label(star_window, text=f"Добрый день. Пожавуста укажите путь к файлу:", font=("Arial Bold", 10))
-# lbl.grid(column=0, row=0)
-# #
-# txt = Entry(star_window, width=30)
-# txt.grid(column=1, row=0)
-# # print(name) #C:/Users/3512/Downloads/Telegram Desktop/v523cas60s-001.fit
-#
-#
-# # source = name
-# hdulist = pyfits.open("C:/Users/3512/Downloads/Telegram Desktop/v523cas60s-001.fit")
-#
-#
-#
-# # chk_state = IntVar()
-# # chk_state.set(0)
-# # chk = Checkbutton(star_window, text='Выбрать', var=chk_state)
-# # chk.grid(column=1, row=0)
-# # #
-# # # btn = Button(window, text="Ок", command=y_or_n)
-# # # btn.grid(column=1, row=2)
-# # #
-# # selected =IntVar()
-# # rad1 = Radiobutton(star_window,text='a', value=1, variable=selected)
-# # rad2 = Radiobutton(star_window, text='b', value=2, variable=selected)
-# # rad3 = Radiobutton(star_window, text='c', value=3, variable=selected)
-# # rad1.grid(column=3, row=0)
-# # rad2.grid(column=3, row=1)
-# # rad3.grid(column=3, row=2)
-# # btn2 = Button(star_window, text="Select", command=select)
-# # btn2.grid(column=3, row=3)
-# # #
-# # #
-# # txt = scrolledtext.ScrolledText(star_window,width=40,height=10)
-# # txt.insert(INSERT, 'Добрый день. Введите путь к файлу, поЖалУвуСта')
-# # txt.grid(column=7, row=5)
-#
-# # btn3 = Button(star_window, text='Вызвать окно', command=mess)
-# # btn3.grid(column=7, row=0)
-
-
-
-
-#
-# #                    Построение графиков
-# hdulist.close()
